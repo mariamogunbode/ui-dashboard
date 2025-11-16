241cdcf0a8d930967f2fc6fb434d7081ab7399ca
Mariam <mariamogunbode@gmail.com>
2025-11-16 20:04:28 +0100

    added text to entirepage component

--- src/EntirePage/EntirePage.jsx
+++ src/EntirePage/EntirePage.jsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { SideBar } from "../sidebar/SideBar";
 
 export function EntirePage() {
-  console.log("EntirePage component rendered");
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
   return (
     <>
       <SideBar />
         <div>
-        <h1>Welcome to the Entire Page, click on pages to view my designs</h1>
+        {isHomePage &&<h1>Welcome to the Entire Page, click on pages to view my designs. And sorry in advance,my sidebar doesn't look attractive yet</h1>}
+
         <Outlet />
       </div>
     
